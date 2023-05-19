2e74d58d7084b0dacda9a14bb936dab092e1d755
Cau <cau.ugolini@gmail.com>
2023-05-18 23:47:12 -0300

    try

--- ck/next.config.js
+++ ck/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true, //this
+    swcMinify: true, //this
     compiler: {
         styledComponents: true,
     }
